3a75af38721b42af91fd03aa3c473ea5d7e9433f
Dmitry Shchepinsky <dmitry.shchepinsky@agileengine.com>
2018-11-24 16:53:11 +0200

    add webpack bundle

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path')
+
+module.exports = {
+  entry: './index.js',
+  mode: 'production',
+  target: 'node',
+  output: {
+    // options related to how webpack emits results
+    path: path.resolve(__dirname, "dist"), // string
+    // the target directory for all output files
+    // must be an absolute path (use the Node.js path module)
+    filename: "app.js", // string    // the filename template for entry chunks
+  }
+}
