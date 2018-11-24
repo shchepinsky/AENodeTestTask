a79d29c36511fdcb1b4d3a95d9589705491930fa
Dmitry Shchepinsky <dmitry.shchepinsky@agileengine.com>
2018-11-24 16:27:42 +0200

    update algorithm to use all element attributes

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const _ = require('lodash')
 const fs = require('fs')
 const {JSDOM} = require('jsdom')
 
+const ARGS_NAMES = {
+  ORIGIN: '--origin',
+  ID: '--id',
+  DIFF: '--diff'
+}
+
 let originFileName
 let originElementId
 let diffFileName
@@ -9,23 +15,23 @@ let diffFileName
 function processArgs () {
   for (const arg of process.argv) {
     const [key, value] = arg.split('=')
-    if (key === '--origin') {
+    if (key === ARGS_NAMES.ORIGIN) {
       originFileName = value
     }
-    if (key === '--id') {
+    if (key === ARGS_NAMES.ID) {
       originElementId = value
     }
-    if (key === '--diff') {
+    if (key === ARGS_NAMES.DIFF) {
       diffFileName = value
     }
   }
 
   if (!originFileName || !originElementId || !diffFileName) {
     console.log(
-      `you must specify arguments\n` +
-      `--origin="some-input-file"` +
-      `--id="some-tag-id"`,
-      `--diff="other file"`,
+      `You must specify arguments` +
+      `\n ${ARGS_NAMES.ID}=some-tag-id`,
+      `\n ${ARGS_NAMES.ORIGIN}=input-file.html` +
+      `\n ${ARGS_NAMES.DIFF}=other-file.html`,
     )
 
     process.exit(-1)
@@ -48,14 +54,22 @@ function processData () {
 }
 
 function makeElementInfo (element) {
-  return {
-    tagName: `${element.tagName}`.toLowerCase(),
-    classes: `${element.className}`.split(' ').map(c => c.toLowerCase()),
-    title: `${element.title}`.toLowerCase(),
-    id: `${element.id}`.toLowerCase(),
-    href: element.href,
-    text: `${element.textContent}`.trim().toLowerCase(),
+  const result = {}
+
+  for(const attr of element.attributes) {
+    let value
+
+    if (attr.name === 'class') {
+      // class names will be compared by each class name
+      value = `${attr.value}`.toLowerCase().split(' ')
+    } else {
+      value = `${attr.value}`.toLowerCase()
+    }
+
+    result[attr.name] = value
   }
+
+  return result
 }
 
 function similarity (e1, e2) {
@@ -68,7 +82,7 @@ function similarity (e1, e2) {
   for (const propName of propNames) {
     const propValue = info1[propName]
     if (Array.isArray(propValue)) {
-      similarity += _.intersection(info1.classes, info2.classes).length
+      similarity += _.intersection(info1[propName], info2[propName]).length
     } else {
       if (info1[propName] === info2[propName]) {
         similarity++
@@ -104,7 +118,6 @@ function traverse (element, target, matchList, path = [], index) {
   path.pop()
 }
 
-// run: // node <your_bundled_script>.js <input_origin_file_path> <input_other_sample_file_path>
 function main () {
   try {
     processArgs()
